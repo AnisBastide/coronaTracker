ca7cc51933efbb4752d47c2e62ecf9b20efc1132
anis bastide <anis.bastide@gmail.com>
2022-02-24 17:16:48 +0100

    added department list and chart

--- CoronaTracker/CityListTableViewController.swift
+++ CoronaTracker/CityListTableViewController.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class CityListTableViewController: UITableViewController {
-    
+    let departement = ["Ain", "Aisne", "Allier", "Alpes-de-Haute-Provence", "Hautes-Alpes", "Alpes-Maritimes", "Ardeche", "Ardennes", "Ariege", "Aube", "Aude", "Aveyron", "Bouches-du-Rhône", "Calvados", "Cantal", "Charente", "Charente-Maritime", "Cher", "Correze", "Corse-du-Sud", "Haute-Corse", "Côte-d'Or", "Côtes d'Armor", "Creuse", "Dordogne", "Doubs", "Drôme", "Eure", "Eure-et-Loir", "Finistere", "Gard", "Haute-Garonne", "Gers", "Gironde", "Herault", "Ille-et-Vilaine", "Indre", "Indre-et-Loire", "Isere", "Jura", "Landes", "Loir-et-Cher", "Loire", "Haute-Loire", "Loire-Atlantique", "Loiret", "Lot", "Lot-et-Garonne", "Lozere", "Maine-et-Loire", "Manche", "Marne", "Haute-Marne", "Mayenne", "Meurthe-et-Moselle", "Meuse", "Morbihan", "Moselle", "Nievre", "Nord", "Oise", "Orne", "Pas-de-Calais", "Puy-de-Dôme", "Pyrenees-Atlantiques", "Hautes-Pyrenees", "Pyrenees-Orientales", "Bas-Rhin", "Haut-Rhin", "Rhône", "Haute-Saône", "Saône-et-Loire", "Sarthe", "Savoie", "Haute-Savoie", "Paris", "Seine-Maritime", "Seine-et-Marne", "Yvelines", "Deux-Sevres", "Somme", "Tarn", "Tarn-et-Garonne", "Var", "Vaucluse", "Vendee", "Vienne", "Haute-Vienne", "Vosges", "Yonne", "Territoire de Belfort", "Essonne", "Hauts-de-Seine", "Seine-St-Denis", "Val-de-Marne", "Val-D'Oise", "Guadeloupe", "Martinique", "Guyane", "La Reunion", "Mayotte"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return departement.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = departement[indexPath.row]
+        // Configure the cell...
+
+        return cell
+    }
+    
+    override func tableView(_ tableView : UITableView,didSelectRowAt indexPath:IndexPath){
         var covidDataList = [] as [covidData]
         let dateList = ["11-02-2022","12-02-2022","13-02-2022","14-02-2022","15-02-2022","16-02-2022","17-02-2022"]
         for date in dateList {
-            getDataFromApi(_city:"Paris", _date:date,{covidData in
+            //print(departement[in/dexPath.row])
+            getDataFromApi(_city:departement[indexPath.row], _date:date,{covidData in
                 covidDataList.append(covidData)
                 //print(covidDataList)
                 
@@ -29,36 +59,7 @@ class CityListTableViewController: UITableViewController {
                 }
             })
         }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -105,6 +106,8 @@ class CityListTableViewController: UITableViewController {
     */
     
     func getDataFromApi(_city:String,_date:String,_ completion: @escaping (covidData) -> ()){
+        print(_city)
+        print(_date)
         let url = URL(string:"https://coronavirusapifr.herokuapp.com/data/departement/\(_city)/\(_date)")!
         //print("blblblblblb")
         //print(url)
@@ -119,7 +122,7 @@ class CityListTableViewController: UITableViewController {
                                                             options:.mutableContainers){
                 if let data = json as? [[String:AnyObject]] {
                     //print(data)
-                    let covid = covidData(hosp: data[0]["hosp"] as? NSNumber,incid_hosp: data[0]["incid_hosp"] as? NSNumber,dchosp: data[0]["dchosp"] as? NSNumber ,rea: data[0]["rea"] as? NSNumber,incid_rea: data[0]["incid_rea"] as? NSNumber,reg: data[0]["reg"] as? NSNumber ,lib_dep: data[0]["lib_dep"] as? String,lib_reg: data[0]["lib_reg"] as? String,R: data[0]["R"] as? NSNumber)
+                    let covid = covidData(hosp: data[0]["hosp"] as? NSNumber,incid_hosp: data[0]["incid_hosp"] as? NSNumber,dchosp: data[0]["dchosp"] as? NSNumber ,rea: data[0]["rea"] as? NSNumber,incid_rea: data[0]["incid_rea"] as? NSNumber,reg: data[0]["reg"] as? NSNumber ,lib_dep: data[0]["lib_dep"] as? String,lib_reg: data[0]["lib_reg"] as? String,R: data[0]["R"] as? NSNumber,date:_date)
                     completion(covid)
                 }}
             };
@@ -139,4 +142,5 @@ struct covidData{
     let lib_dep : String?
     let lib_reg : String?
     let R : NSNumber?
+    let date : String?
 }
--- CoronaTracker/DetailViewController.swift
+++ CoronaTracker/DetailViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Charts
 import TinyConstraints
+import SwiftUI
 class DetailViewController: UIViewController,ChartViewDelegate {
     var covidDataList:[covidData] = []
     lazy var lineChartView : LineChartView = {
@@ -17,16 +18,19 @@ class DetailViewController: UIViewController,ChartViewDelegate {
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(covidDataList)
+        //print(covidDataList)
         view.addSubview(lineChartView)
         lineChartView.centerInSuperview()
         lineChartView.width(to:view)
         lineChartView.heightToWidth(of:view)
         var values:[ChartDataEntry] = []
+        var day = 1
         for covidData in covidDataList {
-            values.append(ChartDataEntry(x:covidData.hosp as? Double ?? 0,y: covidData.R as? Double ?? 0))
+            values.append(ChartDataEntry(x:Double(day),y: covidData.hosp as? Double ?? 0 ))
+            print(day)
+            day+=1
         }
-        setData(values:values)
+        setData(values: values)
         // Do any additional setup after loading the view.
     }
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
@@ -34,19 +38,21 @@ class DetailViewController: UIViewController,ChartViewDelegate {
     }
     func setData (values:[ChartDataEntry]) {
     let set1 = LineChartDataSet(entries: values, label: "Covid")
+    set1.mode = .cubicBezier
+    set1.drawCirclesEnabled = false
+    set1.lineWidth = 3
+    set1.setColor(.white)
+    set1.fillAlpha = 0.8
+    set1.drawFilledEnabled = true
     let data = LineChartData(dataSet: set1)
+    data.setDrawValues(false);
     lineChartView.data = data
     }
 //    let values:[ChartDataEntry] = [
-//        ChartDataEntry(x:0.0,y:10.0),
-//        ChartDataEntry(x:10.0,y:20.0),
-//        ChartDataEntry(x:20.0,y:30.0),
-//        ChartDataEntry(x:30.0,y:40.0),
-//        ChartDataEntry(x:40.0,y:50.0),
-//        ChartDataEntry(x:50.0,y:60.0),
-//        ChartDataEntry(x:60.0,y:70.0),
-//        ChartDataEntry(x:70.0,y:80.0),
-//        ChartDataEntry(x:80.0,y:90.0),
+//        ChartDataEntry(x: 0.0, y: 10.0),
+//        ChartDataEntry(x: 1.0, y: 5.0),
+//
+//
 //    ]
 }
 
